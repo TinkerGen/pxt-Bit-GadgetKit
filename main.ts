f04b2f090b78eb081e3d32ed4fdf35569b8ed9a1
MiroChao <531414952@qq.com>
2019-04-23 16:38:52 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+/*****************************************************************************
+* | Description :	This pxt will load the following extensions for micro:bit
+                    - "BitMaker": "github:MiroChao/pxt-BitMaker",                    
+                    - "chmakered-grove": "github:MiroChao/pxt-CHMakerEd-Grove",
+                    - "neopixel": "github:microsoft/pxt-neopixel#v0.6.10"
+* | Developer   :   CH Makered
+* | More Info   :	http://chmakered.com/
+******************************************************************************/
+
 basic.showLeds(`
     . . . . .
     . # . # .
